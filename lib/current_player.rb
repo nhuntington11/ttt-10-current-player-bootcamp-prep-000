12f00c6562991e02a9670932f57233dc6eac57d4
Nick H <nhuntington11@gmail.com>
2018-07-17 11:05:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,10 @@ def turn_count(board)
   return turn_count
 end
 
-def current_player(integer)
-  if 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
 end
