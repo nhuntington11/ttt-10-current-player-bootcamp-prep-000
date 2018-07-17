6b265d2d5254bca89276ff3659de40252ad6cf40
Nick H <nhuntington11@gmail.com>
2018-07-17 10:58:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", "X", "O", " "]
 
 def turn_count(board)
   turn_count = 0
-  board.each{|turn| if turn == "X" || turn == "O" turn_count += 1}
+  board.each{|turn| if turn == "X" || turn == "O"; turn_count += 1}
   
   puts turn_count  
 end
